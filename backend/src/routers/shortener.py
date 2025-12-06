1cbb4db4a1dfb30e3f300159638902391fcd5e5e
Kacper Kucharski <kacper.2.kucharski@student.uj.edu.pl>
2025-12-06 16:44:14 +0100

    added metrics (not working yet)

--- backend/src/routers/shortener.py
+++ backend/src/routers/shortener.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from controllers.shortener_controller import (
     get_url,
     save_url,
@@ -9,13 +9,16 @@ shortener_router = Blueprint("url", __name__)
 @shortener_router.get("/url/<path:id>")
 def get_shortened_url(id: str):
     url = get_url(id)
-    return {"shortened_url": f"http://short.url/{url}"}, 200
+    if not url:
+        return {"error": "url not found"}, 404
+    return {"shortened_url": url}, 200
 
 
 @shortener_router.post("/url")
 def create_shortened_url():
-    save_url
-    return {"shortened_url": "http://short.url/abc123"}, 201
+    body = request.get_json()
+    shortened_url = save_url(body.get("url"))
+    return {"url" : shortened_url}, 201
 
 @shortener_router.delete("/url/<path:url>")
 def delete_shortened_url(url: str):
