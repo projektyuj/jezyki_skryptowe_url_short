b2dbb08aebb165891d2c3861d1450b5fb9ee08c4
kierasuj <jakub.kierasinski@student.uj.edu.pl>
2026-01-05 20:22:24 +0100

    Fix touters import path

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from routers.router import all_blueprints
+from backend.src.routers.router import all_blueprints
 from .config import Config
 
 def create_app() -> Flask:
